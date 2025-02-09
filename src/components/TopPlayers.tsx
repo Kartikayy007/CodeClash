37e9ace2ce5ad66c96b095042347793b89c09b8b
Shreyanshu005 <shreyanshu23153035@akgec.ac.in>
2025-02-09 17:21:46 +0530

    modified top players

--- src/components/TopPlayers.tsx
+++ src/components/TopPlayers.tsx
@@ -7,7 +7,7 @@ interface TopPlayersProps {
 const TopPlayers = ({ topPlayers }: TopPlayersProps) => (
   <div className="grid grid-cols-3 gap-4 mb-8 relative">
     {topPlayers.map((player, index) => (
-      <div key={index} className={`bg-[#212733] rounded-lg p-4 flex items-center gap-4 mt-8 ${index === 1 ? 'shadow-lg mb-6 mt-0' : ''}`}>
+      <div key={index} className={`bg-[#212733] rounded-lg p-4 flex items-center gap-4 mt-8 ${index === 1 ? 'shadow-lg mb-6 mt-[0px]' : ''}`}>
         <div className="relative w-12 h-12">
           <Image
             src={`/${index === 1 ? 'gold' : index === 0 ? 'silver' : 'bronze'}.svg`}
